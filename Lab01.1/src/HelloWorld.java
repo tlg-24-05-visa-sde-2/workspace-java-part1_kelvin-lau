1ef795ec13787dd8f305991f4562ee586f6b5baf
Kelvin Lau <kelvinl@tlgcohort.com>
2024-06-27 10:46:09 -0500

    Initial commit

---
+++ Lab01.1/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * Classic Hello World application in Java.
+ * This is a standalone application, i.e, it does not run on an app server.
+ */
+class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Starting point or entry point for every stand-alone Java application
+        System.out.println("Hello from IntelliJ");  // Prints to console
+        System.out.println("This is another print statement.");
+    }
+}
