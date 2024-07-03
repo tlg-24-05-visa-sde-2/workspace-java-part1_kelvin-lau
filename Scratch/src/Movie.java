4b49f5dece1620fa04b1a761eb330dc4b08943cd
Kelvin Lau <kelvinl@tlgcohort.com>
2024-07-03 11:03:13 -0500

    Enum constraint for Movie Genre and Rating

--- Scratch/src/Movie.java
+++ Scratch/src/Movie.java
@@ -2,20 +2,26 @@ class Movie {
     private String title;
     private int releaseYear;
     private double revenue;
-
-    public Movie() {
-
-    }
+    private Rating rating;
+    private Genre genre;
 
     public Movie(String title) {
         this.setTitle(title);
     }
 
-    public Movie(String title, int releaseYear, double revenue) {
-        this(title); //delegate to constructor above me for title
+    public Movie(String title, Genre genre) {
+        this(title);
+        this.setGenre(genre);
+    }
+
+    public Movie(String title, int releaseYear, double revenue, Rating rating, Genre genre) {
+        this(title, genre); //delegate to constructor above me for title
         this.setReleaseYear(releaseYear);
         this.setRevenue(revenue);
+        this.setRating(rating);
     }
+
+    // accessor methods
 
     public String getTitle() {
         return this.title;
@@ -41,7 +47,24 @@ class Movie {
         this.revenue = revenue;
     }
 
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
     public String toString() {
-        return "Movie Title: " + this.getTitle() + ", Movie Release Year: " + this.getReleaseYear() + ", Movie Revenue: $" + this.getRevenue();
+        return "Movie Title: " + this.getTitle() + ", Movie Release Year: " + this.getReleaseYear() +
+                ", Rating: " + this.getRating() + ", Genre: " + this.getGenre() + ", Movie Revenue: $" + this.getRevenue();
     }
 }
--- Scratch/src/MovieClient.java
+++ Scratch/src/MovieClient.java
@@ -1,17 +1,22 @@
 class MovieClient {
     public static void main(String[] args) {
-        Movie movie1 = new Movie("Interstellar", 2014, 681_000_000.82);
+        Movie movie1 = new Movie("Interstellar", 2014, 681_000_000.82, Rating.PG_13, Genre.SCI_FI);
 
         System.out.println(movie1);
-
         System.out.println();
 
-        Movie movie2 = new Movie("Casablanca", 1942, 4_627_000.0);
+        Movie movie2 = new Movie("Casablanca", 1942, 4_627_000.0, Rating.PG_13, Genre.DRAMA);
 
         System.out.println(movie2);
+        System.out.println();
 
         Movie movie3 = new Movie("Coming to America");
 
         System.out.println(movie3);
+        System.out.println();
+
+        Movie movie4 = new Movie("Spirited Away", Genre.ANIME);
+
+        System.out.println(movie4);
     }
 }
