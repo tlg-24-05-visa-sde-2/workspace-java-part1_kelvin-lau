407fb8e27fbffc02104469e4951cfc839ebd6fa7
Kelvin Lau <kelvinl@tlgcohort.com>
2024-07-05 14:19:17 -0500

    Regular expressions

--- Scratch/src/StringTest.java
+++ Scratch/src/StringTest.java
@@ -11,5 +11,12 @@ class StringTest {
         String city2New = city2.substring(0, 3);
         System.out.println(city1New == city2New);
         System.out.println(city1.equals(city2));
+        System.out.println();
+
+        String pattern = "\\d{3}[A-Z]{3}|[A-Z]{3}\\d{4}";
+        String plate1 = "637YOC";
+        System.out.println(plate1.matches(pattern));
+        String plate2 = "AAS1124";
+        System.out.println(plate2.matches(pattern));
     }
 }
