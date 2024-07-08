b78a8d6838ea92c33399ef6230a2f9789ae1779f
Kelvin Lau <kelvinl@tlgcohort.com>
2024-07-08 09:37:06 -0500

    Smart enums for Genre

--- Scratch/src/Genre.java
+++ Scratch/src/Genre.java
@@ -1,3 +1,21 @@
 enum Genre {
-    ANIME, DRAMA, HORROR, ROMANTIC_COMEDY, DOCUMENTARY, MYSTERY, SCI_FI, INTERNATIONAL
+    ANIME("Anime"),
+    DRAMA("Drama"),
+    HORROR("Horror"),
+    ROMANTIC_COMEDY("Romantic Comedy"),
+    DOCUMENTARY("Documentary"),
+    MYSTERY("Mystery"),
+    SCI_FI("Sci-Fi"),
+    INTERNATIONAL("International");
+
+    private String display;
+
+    Genre(String display) {
+        this.display = display;
+    }
+
+    public String getDisplay() {
+        return this.display;
+    }
 }
+
